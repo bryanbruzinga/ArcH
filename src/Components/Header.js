2c5c607d64853d47a62dbbe16661b2d97dc861ac
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-04-02 06:05:32 -0300

    primeiro commit

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
